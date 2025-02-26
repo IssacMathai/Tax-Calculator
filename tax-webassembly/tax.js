1467c417ef912ec8abcd45dad64d6cf61908a9b4
Issac Mathai <ima90@uclive.ac.nz>
2025-02-26 16:57:59 +1300

    Implemented viewing tax
    - Issue was that JS needed to be copied to the html file
    - Also needed to use document not DocumentTimeline for calculateButton event listener

--- tax-webassembly/tax.js
+++ tax-webassembly/tax.js
@@ -1,14 +1,16 @@
-import init, {calculate_tax} from "./pkg/tax_webassembly"
+import init, {calculate_tax} from "/pkg/tax_webassembly.js"
 
 async function run() {
     await init()
 
     function calculateTax() {
+        console.log(0)
         const income = parseFloat(document.getElementById("income").value)
         const tax = calculate_tax(income)
         document.getElementById("result").innerText = `Tax: $${tax.toFixed(2)}`
+        console.log(0)
     }
 
-    DocumentTimeline.getElementById("calculateButton").addEventListener("click", calculateTax)
+    document.getElementById("calculateButton").addEventListener("click", calculateTax)
 }
 run()
